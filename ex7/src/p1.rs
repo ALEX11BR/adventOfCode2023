08016483231745463cc231549068ee1226a24d21
Popa Ioan Alexandru <alexioanpopa11@gmail.com>
2023-12-07 12:13:41 +0200

    day 7 - part 1

---
+++ ex7/src/p1.rs
@@ -0,0 +1,95 @@
+use std::{collections::HashMap, io::stdin};
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+enum HandKind {
+    HighCard,
+    OnePair,
+    TwoPair,
+    ThreeOfAKind,
+    FullHouse,
+    FourOfAKind,
+    FiveOfAKind,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+struct Hand {
+    kind: HandKind,
+    order: i32,
+}
+
+fn card_nr(card: char) -> i32 {
+    match card {
+        'A' => 14,
+        'K' => 13,
+        'Q' => 12,
+        'J' => 11,
+        'T' => 10,
+        c if c.is_ascii_digit() => c.to_digit(10).unwrap_or(0) as i32,
+        _ => 0,
+    }
+}
+
+fn hand_parse(hand: &str) -> Hand {
+    let mut card_order = 0;
+    let mut card_presence = HashMap::new();
+    for c in "23456789AJQKT".chars() {
+        card_presence.insert(c, 0);
+    }
+
+    for c in hand.chars() {
+        card_order = card_order * 15 + card_nr(c);
+        card_presence.get_mut(&c).map(|count| {
+            *count += 1;
+            Some(())
+        });
+    }
+
+    let mut card_appearances = card_presence.values().collect::<Vec<_>>();
+    card_appearances.sort_by(|a, b| b.cmp(a));
+
+    let hand_r = if card_appearances[0] == &5 {
+        HandKind::FiveOfAKind
+    } else if card_appearances[0] == &4 {
+        HandKind::FourOfAKind
+    } else if card_appearances[0] == &3 {
+        if card_appearances[1] == &2 {
+            HandKind::FullHouse
+        } else {
+            HandKind::ThreeOfAKind
+        }
+    } else if card_appearances[0] == &2 {
+        if card_appearances[1] == &2 {
+            HandKind::TwoPair
+        } else {
+            HandKind::OnePair
+        }
+    } else {
+        HandKind::HighCard
+    };
+
+    Hand {
+        kind: hand_r,
+        order: card_order
+    }
+}
+
+fn main() {
+    let mut data = stdin()
+        .lines()
+        .filter_map(|result_line| result_line.ok())
+        .map(|line| {
+            let fields = line
+                .split_ascii_whitespace()
+                .collect::<Vec<_>>();
+            (hand_parse(fields[0]), fields[1].parse::<i32>().unwrap_or(0))
+        })
+        .collect::<Vec<_>>();
+    data.sort_by_key(|a| a.0);
+    
+    let mut result = 0;
+    for i in 0..data.len() {
+        result += (i as i32 + 1) * data[i].1;
+    }
+
+    println!("{result}");
+}
